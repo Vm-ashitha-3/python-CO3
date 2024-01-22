8e26d9554f6174400f348aac5c110c8ba76fed9c
Vm-ashitha-3 <150150652+Vm-ashitha-3@users.noreply.github.com>
2024-01-22 17:51:05 +0530

    Create pkg1.py

---
+++ pkg1.py
@@ -0,0 +1,8 @@
+import college.collegedetails
+import college.department.mca
+import college.department.cse
+
+college.collegedetails.collegedetails()
+college.department.mca.sem1()
+college.department.mca.sem2()
+college.department.cse.cse()
